c5cc32f1073516e8e7a70c8fa732ab6302769978
ayush <iamrealglider@gmail.com>
2024-08-14 23:59:51 +0530

    made cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Product from "./components/product";
 
 function App() {
   let [data, setData] = useState([]);
@@ -35,67 +34,85 @@ function App() {
   }, []);
   return (
     <>
-      <main>
-        <div>
-          {data.map((response, index) => (
-            <div className=" relative w-[200px]" key={index}>
-              <img
-                src={`${response.image.desktop}`}
-                alt=""
-                className="rounded-lg"
-              />
+      <main className="container mx-auto">
+        <h1 className="text-3xl font-bold mt-10 mb-5 ml-16">Desserts</h1>
 
-              <div className="">
-                <button
-                  onClick={() => validation(index)}
-                  className="border gap-3  hover:text-[#CF5630] duration-300 transition-all border-[#CF5630] flex rounded-full py-1 px-3 left-7 text-[15px] font-semibold bg-white  top-[174px]   absolute"
+        <div className="flex justify-around">
+          <div className="w-2/4 grid grid-cols-3 gap-y-10 gap-x-5">
+            {data.map((response, index) => (
+              <div className=" relative w-[200px]" key={index}>
+                <img
+                  src={`${response.image.desktop}`}
+                  alt=""
+                  className="rounded-lg"
+                />
+
+                <div className="">
+                  <button
+                    onClick={() => validation(index)}
+                    className="border gap-3  hover:text-[#CF5630] duration-300 transition-all border-[#CF5630] flex rounded-full py-1 px-3 left-7 text-[15px] font-semibold bg-white  top-[174px]   absolute"
+                  >
+                    <img
+                      src="/images/icon-add-to-cart.svg"
+                      className="mt-1  mr-1"
+                      alt=""
+                      width={16}
+                    />
+                    Add to Cart
+                  </button>
+                </div>
+
+                <div
+                  className={`${
+                    isTrue == false && indexesArray.includes(index)
+                      ? "block"
+                      : "hidden"
+                  }`}
                 >
-                  <img
-                    src="/images/icon-add-to-cart.svg"
-                    className="mt-1  mr-1"
-                    alt=""
-                    width={16}
-                  />
-                  Add to Cart
-                </button>
-              </div>
+                  <button className="border   text-white  gap-9 bg-[#CF5630] duration-300 transition-all border-[#CF5630] flex rounded-full py-1 px-3 left-7 text-[15px] font-semibold  top-[174px]   absolute">
+                    <img
+                      src="/images/icon-decrement-quantity.svg"
+                      className="border  rounded-full py-2 px-1"
+                      alt=""
+                      onClick={() => decrementCounter(index)}
+                    />
+                    {indexesArray.includes(index) ? counter : 0}
+                    <img
+                      src="/images/icon-increment-quantity.svg"
+                      className="border rounded-full py-1  px-1"
+                      onClick={() =>
+                        setCounter((previousCounter) => previousCounter + 1)
+                      }
+                      alt=""
+                    />
+                  </button>
+                </div>
 
-              <div
-                className={`${
-                  isTrue == false && indexesArray.includes(index)
-                    ? "block"
-                    : "hidden"
-                }`}
-              >
-                <button className="border   text-white  gap-9 bg-[#CF5630] duration-300 transition-all border-[#CF5630] flex rounded-full py-1 px-3 left-7 text-[15px] font-semibold  top-[174px]   absolute">
-                  <img
-                    src="/images/icon-decrement-quantity.svg"
-                    className="border  rounded-full py-2 px-1"
-                    alt=""
-                    onClick={() => decrementCounter(index)}
-                  />
-                  {indexesArray.includes(index) ? counter : 0}
-                  <img
-                    src="/images/icon-increment-quantity.svg"
-                    className="border rounded-full py-1  px-1"
-                    onClick={() =>
-                      setCounter((previousCounter) => previousCounter + 1)
-                    }
-                    alt=""
-                  />
-                </button>
+                <p className="text-[#c9aea6] text-[14px] mt-5">
+                  {response.category}
+                </p>
+                <h1 className="font-bold text-[16px]">{response.name}</h1>
+                <p className="text-[#A77667] font-semibold">${response.price}</p>
               </div>
+            ))}
+          </div>
 
-              <p className="text-[#c9aea6] text-[14px] mt-5">
-                {response.category}
+          <div className=" bg-white rounded-sm w-[400px] min-h-48 h-[300px]">
+            <h1 className="text-2xl text-[#BA3D19] font-bold mt-4 ml-5 ">
+              Your Cart (0)
+            </h1>
+            <div className={`${isTrue ===  true? "block" : "hidden"} `}>
+              <img
+                src="/images/illustration-empty-cart.svg"
+                alt=""
+                className="mx-auto mt-9"
+              />
+              <p className="text-center text-[##9C8784] font-medium mt-6">
+                Your added were appear here.
               </p>
-              <h1 className="font-bold text-[16px]">{response.name}</h1>
-              <p className="text-[#A77667] font-semibold">{response.price}</p>
             </div>
-          ))}
+          </div>
         </div>
-
-        <div></div>
       </main>
     </>
   );
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -58,4 +58,4 @@ const Product = () => {
 };
 
 export default Product;
-<s></s>;
+
